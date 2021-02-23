8edcadefe903b319fc9e22ab6bab460ee43e9b6a
Aaditya Chandrasekhar <aadityac@gmail.com>
2021-02-23 13:40:13 -0800

    Switch swift-syntax to val-verde.

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "SwiftSyntax",
-                 url: "https://github.com/apple/swift-syntax.git",
-                 .exact("0.50300.0")),
+                 url: "https://github.com/val-verde/swift-syntax.git",
+                 .branch("val-verde-mainline")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
