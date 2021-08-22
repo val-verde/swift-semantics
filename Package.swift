ac7b33e11154120dfdb62a1f57ff6470ff779b6f
Aaditya Chandrasekhar <aadityac@gmail.com>
2021-08-22 12:24:03 -0700

    Prefer system swift-syntax in Package.swift.

--- Package.swift
+++ Package.swift
@@ -9,21 +9,22 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftSemantics",
+            type: .dynamic,
             targets: ["SwiftSemantics"]
         ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "SwiftSyntax",
-                 url: "https://github.com/val-verde/swift-syntax.git",
-                 .branch("val-verde-mainline")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftSemantics",
-            dependencies: ["SwiftSyntax"]
+            dependencies: [],
+            linkerSettings: [
+                .linkedLibrary("SwiftSyntax"),
+            ]
         ),
         .testTarget(
             name: "SwiftSemanticsTests",
